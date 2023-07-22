0975a1a12f5e2cbba1e7e85936a14f2142ffb6b2
chandan <chandankl0310@gmail.com>
2023-07-23 00:51:55 +0530

    feat: Add button disble support

--- src/modules/common/components/Button/Button.jsx
+++ src/modules/common/components/Button/Button.jsx
@@ -6,15 +6,16 @@ function Button({
   text,
   onClick,
   className = '',
+  disabled = false,
   showLoading = false,
   loadingText = 'Loading..'
 }) {
   return (
-    <button className={`${styles.button} ${className}`} onClick={onClick}>
+    <button className={`${styles.button} ${className}`} onClick={onClick} disabled={disabled}>
       {!showLoading ? text : null}
       {showLoading ? loadingText : null}
     </button>
   )
 }
 
-export default Button
+export default React.memo(Button)
