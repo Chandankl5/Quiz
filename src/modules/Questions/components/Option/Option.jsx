49f9ab9a1b22eb27ca59244aeacd4f3aae21846b
chandan <chandankl0310@gmail.com>
2023-07-23 13:20:09 +0530

    Integrate Icon Components

--- src/modules/Questions/components/Option/Option.jsx
+++ src/modules/Questions/components/Option/Option.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import styles from "./Option.module.css";
+import CircleUncheck from "@/modules/common/components/Icons/CircleUncheck";
+import CircleCheck from "@/modules/common/components/Icons/CircleCheck";
 
 function Option({ id, label, selected }) {
   return (
@@ -9,35 +11,9 @@ function Option({ id, label, selected }) {
       className={`${styles.option} ${selected ? styles.optionSelected : ""}`}
     >
       {!selected ? (
-        <svg
-          className={styles.radioVirtual}
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-          fill="none"
-        >
-          <path
-            opacity="0.0829846"
-            d="M46 24C46 36.1503 36.1503 46 24 46C11.8497 46 2 36.1503 2 24C2 11.8497 11.8497 2 24 2C36.1503 2 46 11.8497 46 24Z"
-            stroke="black"
-            strokeWidth="4"
-          />
-        </svg>
+        <CircleUncheck className={styles.radioVirtual} />
       ) : (
-        <svg
-          className={styles.radioVirtualSelected}
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-          fill="none"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M48 24C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24C0 10.7452 10.7452 0 24 0C37.2548 0 48 10.7452 48 24ZM37.3137 14.3431L39.435 16.4645L25.2929 30.6066L22.4645 33.435L19.636 30.6066L12.565 23.5355L14.6863 21.4142L22.38 28.0821L37.3137 14.3431Z"
-            fill="#44B77B"
-          />
-        </svg>
+        <CircleCheck className={styles.radioVirtualSelected} />
       )}
       <span className={styles.label}>{label}</span>
     </div>
