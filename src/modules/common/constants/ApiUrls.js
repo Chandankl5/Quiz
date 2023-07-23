52a1ee18498f920c7bea0fe1a2f58c69975f7cff
chandan <chandankl0310@gmail.com>
2023-07-23 21:29:45 +0530

    Add API URl for result

--- src/modules/common/constants/ApiUrls.js
+++ src/modules/common/constants/ApiUrls.js
@@ -1,6 +1,7 @@
 const ApiUrls =  {
   quiz(quizId)  { return `/api/quizzes/${quizId}`},
-  questions(quizId, questionId) { return `/api/quizzes/${quizId}/questions/${questionId}` }
+  questions(quizId, questionId) { return `/api/quizzes/${quizId}/questions/${questionId}` },
+  result(quizId) { return `/api/quizzes/${quizId}/result`}
 }
 
 export default ApiUrls;
