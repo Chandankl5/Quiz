0b0eb1a92f7c4fd1b346b0594f027e10c9b9c112
chandan <chandankl0310@gmail.com>
2023-07-23 21:29:30 +0530

    feat: Add Arrow, CircleCLip and Meter Components

---
+++ src/modules/common/components/Icons/CircleClip.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function CircleClip({
+  className
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 480 348"
+      fill="none"
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M25.3029 347.379L240.059 240L454.716 347.342C470.895 315.042 480 278.584 480 240C480 107.452 372.548 0 240 0C107.452 0 0 107.452 0 240C0 278.599 9.11223 315.07 25.3029 347.379Z"
+        fill="#EBEDF5"
+      />
+    </svg>
+  );
+}
+
+export default CircleClip;
