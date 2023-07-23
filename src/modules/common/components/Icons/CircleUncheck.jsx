13bc62433a902262375e5d2087cb3de24151518d
chandan <chandankl0310@gmail.com>
2023-07-23 13:19:43 +0530

    feat: Add Circle Check & Circle Uncheck Icons

---
+++ src/modules/common/components/Icons/CircleUncheck.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function CircleUncheck({
+  className
+}) {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      className={className}
+    >
+      <path
+        opacity="0.0829846"
+        d="M46 24C46 36.1503 36.1503 46 24 46C11.8497 46 2 36.1503 2 24C2 11.8497 11.8497 2 24 2C36.1503 2 46 11.8497 46 24Z"
+        stroke="black"
+        strokeWidth="4"
+      />
+    </svg>
+  );
+}
+
+export default CircleUncheck;
